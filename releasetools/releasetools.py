CyanogenMod/android_device_sony_yukon

init: use releasetools to set the firmware symlinks
All devices have a variant specific LTALabel partition. Mount it
and find the variant from the html file on this partition.

Thanks @invisiblek for showing me this exists
https://github.com/CyanogenMod/android_device_htc_m8-common/tree/cm-13.0/releasetools

Change-Id: I5c6db61db74bad79d27a16a0ee32e234bb733d08

releasetools/releasetools.py
@@ -1,0 +1,27 @@
+# Copyright (C) 2012 The Android Open Source Project
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+""" Custom OTA commands for yukon devices """
+
+import common
+import re
+import os
+
+def FullOTA_InstallEnd(info):
+  info.script.Mount("/system")
+  info.script.AppendExtra('assert(run_program("/tmp/install/bin/firmware.sh") == 0);')
+  info.script.Unmount("/system")
+

galaxyfreak/device-sony-common

releasetools/releasetools.py
@@ -1,0 +1,27 @@
+# Copyright (C) 2012 The Android Open Source Project
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+""" Custom OTA commands for Sony devices """
+
+import common
+import re
+import os
+
+def FullOTA_InstallEnd(info):
+  info.script.Mount("/system")
+  info.script.AppendExtra('assert(run_program("/tmp/install/bin/firmware.sh") == 0);')
+  info.script.Unmount("/system")
+